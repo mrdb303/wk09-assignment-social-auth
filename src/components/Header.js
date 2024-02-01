4a6bc3ada134ffc80fd181674b349a954c664af6
mrdb303 <dannybrien@ntlworld.com>
2024-02-01 13:29:29 +0000

    login tests

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,17 @@ import {
 export default function Header() {
   return (
     <header style={{ display: "flex", justifyContent: "space-between", padding: 20 }}>
-      <SignedIn>
-        {/* Mount the UserButton component */}
+      
+      {/* <SignedIn>
+        
         <UserButton />
       </SignedIn>
       <SignedOut>
-        {/* Signed out users get sign in button */}
+        
         <SignInButton/>
-      </SignedOut>
+      </SignedOut> */}
+
+
     </header>
   );
 }
